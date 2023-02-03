1a8402c5dfa77456f0cd7948fe7a270b68b7050b
Ason Uthatham <41178332+Alhzz@users.noreply.github.com>
2023-02-03 22:37:06 +0700

    First

---
+++ main.py
@@ -0,0 +1,59 @@
+from torchvision import transforms
+from torch.autograd import Variable
+from PIL import Image
+import torch
+import io
+import json
+from flask import Flask, jsonify, request, json
+
+""" Prepre necesary thing """
+# FLASK_ENV=development FLASK_APP=inference.py flask run
+app = Flask(__name__)
+
+# Name of an exist class
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # we will get the file from the request
+        file = request.form['file']
+        class_names = ['Alstonia', 'Alyxia', 'Andrographis', 'Antidesma', 'Averrhoa', 'Capparis', 'Cardiospermum', 'Cinchona', 'CordylineA', 'CordylineB', 'Crateva', 'Eurycoma', 'Ficus', 'Garcinia', 'Graptophyllum', 'Hrrisonia', 'Tacca', 'Tiliacora', 'Tinospora', 'Uvaria']
+
+        # Check if a GPU is available
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        # Transform input image into a proper model's input shape
+        loader = transforms.Compose([transforms.Resize(256), 
+                                    transforms.ToTensor(), 
+                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                                    ])
+
+        def image_loader(image_name):
+            # load and manage an image
+            image = Image.open(image_name).convert('RGB')
+            image = loader(image)
+            image = image.float()
+            image = Variable(image, requires_grad=True)
+            return image.to(device)
+
+
+
+        """ Detection section """
+        # Load model
+        # model_path = "./model/resnet.pth"
+        model_path = "./model/efficientnet2.pth"
+        model = torch.load(model_path, map_location=device)
+
+        # Load image
+        image_path = file
+        image = image_loader(image_path)
+
+        model.eval()
+
+        outputs = model(image.unsqueeze(0))
+        _, preds = torch.max(outputs, 1)
+        print(class_names[preds])
+        return jsonify({'class_name': class_names[preds]})
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
